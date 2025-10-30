3bfd9c5a9c869a155827afb9d57fbab1f5cd8d93
juhinahbatool-dotcom <juhinah.batool@nu.edu.pk>
2025-10-30 11:54:59 +0500

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import zipfile
 # CONFIGURATION
 # -------------------------------
 # Replace this with your actual Google Drive shareable link
-DRIVE_FILE_URL = "https://drive.google.com/file/d/1rMqY_RfLSY_Y-OnQ9wM4fW25kPRVZs_C/view?usp=sharing"
+DRIVE_FILE_URL = "1rMqY_RfLSY_Y-OnQ9wM4fW25kPRVZs_C"
 MODEL_ZIP_PATH = "1rMqY_RfLSY_Y-OnQ9wM4fW25kPRVZs_C"
 MODEL_DIR = "fine_tuned_gpt2_recipes"
 
